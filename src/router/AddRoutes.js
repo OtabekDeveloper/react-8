54ee9e5fa4e0a7ce6d32d9fa74783460c753dad1
Muhammadiy93 <anvarov_m@yahoo.com>
2023-03-09 23:04:54 +0500

    Footer tuyasaldi, page lar boglandi

--- src/router/AddRoutes.js
+++ src/router/AddRoutes.js
@@ -3,6 +3,9 @@ import Navbar from '../components/navbar/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Asosiy from '../view/home/Asosiy'
 import Maxsulotlar from '../view/maxsulotlar/Maxsulotlar'
+import Footer from '../components/navbar/footer/Footer'
+import Karzinka from '../view/karzinka/Karzinka'
+import Xarita from '../view/xarita/Xarita'
 
 
 
@@ -14,8 +17,11 @@ function AddRoutes() {
             
           <Route path='/asosiy' element = { <Asosiy /> } />
           <Route path='/maxsulotlar' element = { <Maxsulotlar /> } />
-        
+          <Route path='/karzinka' element = { <Karzinka /> } />
+          <Route path='/xarita' element = { <Xarita /> } />
+
         </Routes>
+        <Footer/>
     </div>
   )
 }
